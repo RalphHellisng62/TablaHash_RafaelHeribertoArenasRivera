8ce47aeb7d43b1a3eef0276b86183b6e7c3657d8
RalphHellisng62 <arenasrafael889@gmail.com>
2025-11-18 18:38:09 -0600

    Lectura del archivo y carga de nombres al HashMap

--- TablaHash.java
+++ TablaHash.java
@@ -1,12 +1,30 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.HashMap;
 
 public class TablaHash {
-    // Empezamos con la construción de un main
+    // Empezamos con la construcción de un main
     public static void main(String[] args) {
         // nombre del arcihvo
         String archivo = "nombresTablaHash.txt";
         
         // Esto sera la tabla hash para almacenar los nombres
         HashMap<Integer, String> tablaHash = new HashMap<>();
+
+        // Aqui lee el archivo y cargara los nombres al HashMap
+        try (BufferedReader br = new BufferedReader(new FileReader(archivo))) {
+            String linea;
+            int id = 1;
+            
+            while ((linea = br.readLine()) != null) {
+                tablaHash.put(id, linea.trim());
+                id++;
+            }
+        } catch (IOException e) {
+            System.out.println("Error al leer el archivo: " + e.getMessage());
+            return;
+        }
+        
     }
 }
