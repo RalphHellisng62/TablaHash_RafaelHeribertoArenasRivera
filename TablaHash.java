19634a7937ac2db11292ae5cf421ae1ed91a679c
RalphHellisng62 <arenasrafael889@gmail.com>
2025-11-18 18:34:53 -0600

    Construcción del main y la TablaHash

--- TablaHash.java
+++ TablaHash.java
@@ -1,3 +1,12 @@
+import java.util.HashMap;
+
 public class TablaHash {
-    
+    // Empezamos con la construción de un main
+    public static void main(String[] args) {
+        // nombre del arcihvo
+        String archivo = "nombresTablaHash.txt";
+        
+        // Esto sera la tabla hash para almacenar los nombres
+        HashMap<Integer, String> tablaHash = new HashMap<>();
+    }
 }
